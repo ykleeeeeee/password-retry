9b9abb5407d47743102855f63fa747e4b7543d19
Yklee <yklee523@gail.com>
2023-09-13 21:59:11 +0800

    Update password.py

--- password.py
+++ password.py
@@ -1,13 +1,15 @@
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
+    i = i - 1
     pwd = input('請輸入密碼:')          #存入的字串名稱不能與上line1的password重複
     if pwd == password:
         print('登入成功')
         break                 #逃出迴圈
     else:
-    	i = i - 1 
-    	print('密碼錯誤!  還有', i,'次機會')
-    	if i == 0 :
-    		break
+        print('密碼錯誤!')
+        if i > 0 :
+            print('  還有', i, '次機會')
+        else:
+            print('帳號被鎖定')
 
